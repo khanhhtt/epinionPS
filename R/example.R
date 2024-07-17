6249735998c702350af36f24728f923bfefd3e87
khanhhtt <calmthewind@gmai.com>
2024-07-17 16:53:28 +0700

    Adjust description

--- R/example.R
+++ R/example.R
@@ -1,4 +1,4 @@
-#' Get path to epinionRWeighting example
+#' Get path to epinionPS example
 #'
 #' epinionRWeighting comes bundled with a number of sample files in its `inst/extdata`
 #' directory. This function make them easy to access
